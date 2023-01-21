4fe9a7dc5dcfc7bacdc0dfb223395a3f2db43382
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-01-20 20:56:39 -0500

    fixup formmatting

--- src/java/lexer/Tokeniser.java
+++ src/java/lexer/Tokeniser.java
@@ -1,74 +1,50 @@
 package lexer;
-
 import lexer.Token.TokenClass;
-
 import java.io.EOFException;
 import java.io.IOException;
-
 /**
  * @author cdubach
  */
-public class Tokeniser {
-
-    private Scanner scanner;
-
-    private int error = 0;
-    public int getErrorCount() {
+public class Tokeniser{
+  private Scanner scanner;
+  private int error=0;
+  public int getErrorCount(){
 	return this.error;
-    }
-
-    public Tokeniser(Scanner scanner) {
-        this.scanner = scanner;
-    }
-
-    private void error(char c, int line, int col) {
-        System.out.println("Lexing error: unrecognised character ("+c+") at "+line+":"+col);
+  }
+  public Tokeniser(Scanner scanner){
+	this.scanner=scanner;
+  }
+  private void error(char c,int line,int col){
+	System.out.println("Lexing error: unrecognised character ("+c+") at "+line+":"+col);
 	error++;
-    }
-
-
-    public Token nextToken() {
-        Token result;
-        try {
-             result = next();
-        } catch (EOFException eof) {
-            // end of file, nothing to worry about, just return EOF token
-            return new Token(TokenClass.EOF, scanner.getLine(), scanner.getColumn());
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            // something went horribly wrong, abort
-            System.exit(-1);
-            return null;
-        }
-        return result;
-    }
-
-    /*
-     * To be completed
-     */
-    private Token next() throws IOException {
-
-        int line = scanner.getLine();
-        int column = scanner.getColumn();
-
-        // get the next character
-        char c = scanner.next();
-
-        // skip white spaces
-        if (Character.isWhitespace(c))
-            return next();
-
-        // recognises the plus operator
-        if (c == '+')
-            return new Token(TokenClass.PLUS, line, column);
-
-        // ... to be completed
-
-
-        // if we reach this point, it means we did not recognise a valid token
-        error(c, line, column);
-        return new Token(TokenClass.INVALID, line, column);
-    }
-
-
+  }
+  public Token nextToken(){
+	Token result;
+	try{
+	  result=next();
+	}catch(EOFException eof){
+	  // end of file, nothing to worry about, just return EOF token
+	  return new Token(TokenClass.EOF,scanner.getLine(),scanner.getColumn());
+	}catch(IOException ioe){
+	  ioe.printStackTrace();
+	  // something went horribly wrong, abort
+	  System.exit(-1);
+	  return null;
+	}
+	return result;
+  }
+  // todo
+  private Token next()throws IOException{
+	int line=scanner.getLine();
+	int column=scanner.getColumn();
+	char c=scanner.next();
+	if(Character.isWhitespace(c)) //todo don't recurse, cringe
+	  return next();
+	//todo switch, cringe
+	if(c=='+')
+	  return new Token(TokenClass.PLUS,line,column);
+	// if we reach this point, it means we did not recognise a valid token
+	error(c,line,column);
+	return new Token(TokenClass.INVALID,line,column);
+  }
 }
--- src/java/parser/Parser.java
+++ src/java/parser/Parser.java
@@ -1,155 +1,106 @@
 package parser;
-
-
 import lexer.Token;
 import lexer.Token.TokenClass;
 import lexer.Tokeniser;
-
 import java.util.LinkedList;
 import java.util.Queue;
-
-
 /**
  * @author cdubach
  */
-public class Parser {
+public class Parser{
+  private Token token;
+  private Queue<Token>buffer=new LinkedList<>();
+  private final Tokeniser tokeniser;
+  private int error=0;
+  private Token lastErrorToken;
+  public Parser(Tokeniser tokeniser){
+	this.tokeniser=tokeniser;
+  }
+  public void parse(){
+	nextToken();//get the first token
+	parseProgram();
+  }
+  public int getErrorCount(){
+	return error;
+  }
+  private void error(TokenClass...expected){
+	if(lastErrorToken==token)return;//skip error, same token
+	StringBuilder sb=new StringBuilder();
+	String sep="";
+	for(TokenClass e:expected){
+	  sb.append(sep);
+	  sb.append(e);
+	  sep="|";
+	}
+	System.out.println("Parsing error: expected ("+sb+") found ("+token+") at "+token.position);
+	error++;
+	lastErrorToken=token;
+  }
 
-    private Token token;
-
-    private Queue<Token> buffer = new LinkedList<>();
-
-    private final Tokeniser tokeniser;
-
-
-
-    public Parser(Tokeniser tokeniser) {
-        this.tokeniser = tokeniser;
-    }
-
-    public void parse() {
-        // get the first token
-        nextToken();
-
-        parseProgram();
-    }
-
-    public int getErrorCount() {
-        return error;
-    }
-
-    private int error = 0;
-    private Token lastErrorToken;
-
-    private void error(TokenClass... expected) {
-
-        if (lastErrorToken == token) {
-            // skip this error, same token causing trouble
-            return;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        String sep = "";
-        for (TokenClass e : expected) {
-            sb.append(sep);
-            sb.append(e);
-            sep = "|";
-        }
-        System.out.println("Parsing error: expected ("+sb+") found ("+token+") at "+token.position);
-
-        error++;
-        lastErrorToken = token;
-    }
-
-    /*
-     * Look ahead the i^th element from the stream of token.
-     * i should be >= 1
-     */
-    private Token lookAhead(int i) {
-        // ensures the buffer has the element we want to look ahead
-        while (buffer.size() < i)
-            buffer.add(tokeniser.nextToken());
-
-        int cnt=1;
-        for (Token t : buffer) {
-            if (cnt == i)
-                return t;
-            cnt++;
-        }
-
-        assert false; // should never reach this
-        return tokeniser.nextToken();
-    }
-
-
-    /*
-     * Consumes the next token from the tokeniser or the buffer if not empty.
-     */
-    private void nextToken() {
-        if (!buffer.isEmpty())
-            token = buffer.remove();
-        else
-            token = tokeniser.nextToken();
-    }
-
-    /*
-     * If the current token is equals to the expected one, then skip it, otherwise report an error.
-     */
-    private void expect(TokenClass... expected) {
-        for (TokenClass e : expected) {
-            if (e == token.tokenClass) {
-                nextToken();
-                return;
-            }
-        }
-        error(expected);
-    }
-
-    /*
-    * Returns true if the current token is equals to any of the expected ones.
-    */
-    private boolean accept(TokenClass... expected) {
-        for (TokenClass e : expected) {
-            if (e == token.tokenClass)
-                return true;
-        }
-        return false;
-    }
-
-
-    private void parseProgram() {
-        parseIncludes();
-
-        while (accept(TokenClass.STRUCT, TokenClass.INT, TokenClass.CHAR, TokenClass.VOID)) {
-            if (token.tokenClass == TokenClass.STRUCT &&
-                    lookAhead(1).tokenClass == TokenClass.IDENTIFIER &&
-                    lookAhead(2).tokenClass == TokenClass.LBRA) {
-                parseStructDecl();
-            }
-            else {
-                // to be completed ...
-                nextToken(); // this line should be modified/removed
-            }
-        }
-        // to be completed ...
-
-        expect(TokenClass.EOF);
-    }
-
-    // includes are ignored, so does not need to return an AST node
-    private void parseIncludes() {
-        if (accept(TokenClass.INCLUDE)) {
-            nextToken();
-            expect(TokenClass.STRING_LITERAL);
-            parseIncludes();
-        }
-    }
-
-    private void parseStructDecl(){
-        expect(TokenClass.STRUCT);
-        expect(TokenClass.IDENTIFIER);
-        expect(TokenClass.LBRA);
-        // to be completed ...
-    }
-
-    // to be completed ...
+  //look ahead i elements in token stream; i>0
+  private Token lookAhead(int i){
+	while(buffer.size()<i)//ensures the buffer has the element we want
+	  buffer.add(tokeniser.nextToken());
+	int cnt=1;
+	for(Token t:buffer){
+	  if(cnt==i)
+		return t;
+	  cnt++;
+	}
+	assert false; // should never reach this
+	return tokeniser.nextToken();
+  }
+  //Consumes the next token from the tokeniser or the buffer if not empty.
+  private void nextToken(){
+	if(!buffer.isEmpty())
+	  token=buffer.remove();
+	else
+	  token=tokeniser.nextToken();
+  }
+  //next token if cur is the expected, otherwise error
+  private void expect(TokenClass...expected){
+	for(TokenClass e:expected){
+	  if(e==token.tokenClass){
+		nextToken();
+		return;
+	  }
+	}
+	error(expected);
+  }
+  //return whether current token is equal to any expected one
+  private boolean accept(TokenClass...expected){
+	for(TokenClass e : expected){
+	  if (e == token.tokenClass)
+		return true;
+	}
+	return false;
+  }
+  private void parseProgram(){
+	parseIncludes();
+	while(accept(TokenClass.STRUCT,TokenClass.INT,TokenClass.CHAR,TokenClass.VOID)){
+	  if(token.tokenClass==TokenClass.STRUCT&&lookAhead(1).tokenClass==TokenClass.IDENTIFIER&&lookAhead(2).tokenClass==TokenClass.LBRA){
+		parseStructDecl();
+	  }else{
+		//todo
+		nextToken(); //todo this line should be modified/removed
+	  }
+	}
+	//todo
+	expect(TokenClass.EOF);
+  }
+  //includes are ignored, so does not need to return an AST node
+  private void parseIncludes(){
+	if(accept(TokenClass.INCLUDE)){
+	  nextToken();
+	  expect(TokenClass.STRING_LITERAL);
+	  parseIncludes();
+	}
+  }
+  private void parseStructDecl(){
+	expect(TokenClass.STRUCT);
+	expect(TokenClass.IDENTIFIER);
+	expect(TokenClass.LBRA);
+	//todo
+  }
+  //todo
 }
