dd4d2e4f15599864a145c4a288d20b70ed0b9177
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-01-23 22:25:26 -0500

    lexer

--- src/java/lexer/Tokeniser.java
+++ src/java/lexer/Tokeniser.java
@@ -1,18 +1,18 @@
 package lexer;
-import lexer.Token.TokenClass;
 import java.io.EOFException;
 import java.io.IOException;
+import lexer.Token.TokenClass;
 /**
  * @author cdubach
  */
 public class Tokeniser{
-  private Scanner scanner;
+  private Scanner sc;
   private int error=0;
   public int getErrorCount(){
 	return this.error;
   }
-  public Tokeniser(Scanner scanner){
-	this.scanner=scanner;
+  public Tokeniser(Scanner sc){
+	this.sc=sc;
   }
   private void error(char c,int line,int col){
 	System.out.println("Lexing error: unrecognised character ("+c+") at "+line+":"+col);
@@ -22,29 +22,267 @@ public class Tokeniser{
 	Token result;
 	try{
 	  result=next();
-	}catch(EOFException eof){
-	  // end of file, nothing to worry about, just return EOF token
-	  return new Token(TokenClass.EOF,scanner.getLine(),scanner.getColumn());
-	}catch(IOException ioe){
+	}catch(EOFException eof){//eof; not problem, just return EOF token
+	  return new Token(TokenClass.EOF,sc.getLine(),sc.getColumn());
+	}catch(IOException ioe){//something went horribly wrong, abort
 	  ioe.printStackTrace();
-	  // something went horribly wrong, abort
 	  System.exit(-1);
 	  return null;
 	}
 	return result;
   }
-  // todo
   private Token next()throws IOException{
-	int line=scanner.getLine();
-	int column=scanner.getColumn();
-	char c=scanner.next();
-	if(Character.isWhitespace(c)) //todo don't recurse, cringe
-	  return next();
-	//todo switch, cringe
-	if(c=='+')
-	  return new Token(TokenClass.PLUS,line,column);
-	// if we reach this point, it means we did not recognise a valid token
-	error(c,line,column);
-	return new Token(TokenClass.INVALID,line,column);
+	char c=sc.next();
+	while(Character.isWhitespace(c))
+	  c=sc.next();
+	int ln=sc.getLine(),cn=sc.getColumn();
+	StringBuilder v;
+	switch(c){
+	case'{':
+	  return new Token(TokenClass.LBRA,ln,cn);
+	case'}':
+	  return new Token(TokenClass.RBRA,ln,cn);
+	case'(':
+	  return new Token(TokenClass.LPAR,ln,cn);
+	case')':
+	  return new Token(TokenClass.RPAR,ln,cn);
+	case'[':
+	  return new Token(TokenClass.LSBR,ln,cn);
+	case']':
+	  return new Token(TokenClass.RSBR,ln,cn);
+	case';':
+	  return new Token(TokenClass.SC,ln,cn);
+	case',':
+	  return new Token(TokenClass.COMMA,ln,cn);
+	case'=':
+	  if(!sc.hasNext()||sc.peek()!='=')
+		return new Token(TokenClass.ASSIGN,ln,cn);
+	  sc.next();
+	  return new Token(TokenClass.EQ,ln,cn);
+	case'!':
+	  if(!sc.hasNext()||sc.peek()!='='){//no unary not operation i guess?
+		error(c,ln,cn);
+		return new Token(TokenClass.INVALID,ln,cn);
+	  }
+	  sc.next();
+	  return new Token(TokenClass.NE,ln,cn);
+	case'<'://no bitshifts?
+	  if(!sc.hasNext()||sc.peek()!='=')
+		return new Token(TokenClass.LT,ln,cn);
+	  sc.next();
+	  return new Token(TokenClass.LE,ln,cn);
+	case'>':
+	  if(!sc.hasNext()||sc.peek()!='=')
+		return new Token(TokenClass.GT,ln,cn);
+	  sc.next();
+	  return new Token(TokenClass.GE,ln,cn);
+	case'+'://no increment, add assigns?
+	  return new Token(TokenClass.PLUS,ln,cn);
+	case'-'://no --, -=, ->
+	  return new Token(TokenClass.MINUS,ln,cn);
+	case'*':
+	  return new Token(TokenClass.ASTERIX,ln,cn);
+	case'%':
+	  return new Token(TokenClass.REM,ln,cn);
+	case'&':
+	  if(!sc.hasNext()||sc.peek()!='&')
+		return new Token(TokenClass.AND,ln,cn);
+	  sc.next();
+	  return new Token(TokenClass.LOGAND,ln,cn);
+	case'|':
+	  if(!sc.hasNext()||sc.peek()!='|'){//no bitwise or?
+		error(c,ln,cn);
+		return new Token(TokenClass.INVALID,ln,cn);
+	  }
+	  sc.next();
+	  return new Token(TokenClass.LOGOR,ln,cn);
+	case'.':
+	  return new Token(TokenClass.DOT,ln,cn);
+	case'/':
+	  if(sc.hasNext())
+		if(sc.peek()=='/'){
+		  while(c!='\n')c=sc.next();//eof without newline means no more tokens
+		  return next();
+		}else if(sc.peek()=='*'){
+		  sc.next();//need to consume the first one or we recognize /*/
+		  while(sc.hasNext())
+			if(sc.next()=='*'&&sc.hasNext()&&sc.peek()=='/'){//could have **/
+			  sc.next();
+			  return next();
+			}
+		  System.err.println("Reached eof without closing block comment");
+		  error(c,ln,cn);
+		  return new Token(TokenClass.INVALID,ln,cn);
+		}
+	  return new Token(TokenClass.DIV,ln,cn);
+	case'#':
+	  v=new StringBuilder();
+	  while(sc.hasNext()&&Character.isLetter(sc.peek()))
+		v.append(sc.next());
+	  if(!v.toString().equals("include")){
+		System.err.println("Unrecognized preprosser (sp.) directive");
+		error(c,ln,cn);
+		return new Token(TokenClass.INVALID,ln,cn);
+	  }
+	  v=new StringBuilder();
+	  while(sc.hasNext()&&sc.peek()!='\n'&&Character.isWhitespace(sc.peek()))
+		sc.next();//in case whitespace before directive
+	  while(sc.hasNext()&&sc.peek()!='\n')
+		v.append(sc.next());//consume filename to include, just rest of line?
+	  return new Token(TokenClass.INCLUDE,v.toString(),ln,cn);
+	case'"':
+	  v=new StringBuilder();
+	  while(sc.hasNext()){
+		c=sc.next();
+		switch(c){
+		case'"':
+		  return new Token(TokenClass.STRING_LITERAL,v.toString(),ln,cn);
+		case'\\':
+		  switch(sc.next()){
+		  case't':
+			v.append('\t');
+			break;
+		  case'b':
+			v.append('\b');
+			break;
+		  case'n':
+			v.append('\n');
+			break;
+		  case'f':
+			v.append('\f');
+			break;
+		  case'\\':
+			v.append('\\');
+			break;
+		  case'"':
+			v.append('\"');
+			break;
+		  case'\'':
+			v.append('\'');
+			break;
+		  case'0':
+			v.append(0);
+			break;
+		  default:
+			System.err.println("Unrecognized escape code in string literal");
+			error(c,ln,cn);
+			return new Token(TokenClass.INVALID,ln,cn);
+		  }
+		  break;
+		default:
+		  v.append(c);
+		}
+	  }
+	  System.err.println("Unclosed string");
+	  error(c,ln,cn);
+	  return new Token(TokenClass.INVALID,ln,cn);
+	case'\'':
+	  v=new StringBuilder();
+	  while(sc.hasNext()){
+		c=sc.next();
+		switch(c){
+		case'\'':
+		  if(v.length()==1)
+			return new Token(TokenClass.CHAR_LITERAL,v.toString(),ln,cn);
+		  System.err.println("Char literal consisting of multiple characters");
+		  error(c,ln,cn);
+		  return new Token(TokenClass.INVALID,ln,cn);
+		case'\\':
+		  switch(sc.next()){
+		  case't':
+			v.append('\t');
+			break;
+		  case'b':
+			v.append('\b');
+			break;
+		  case'n':
+			v.append('\n');
+			break;
+		  case'f':
+			v.append('\f');
+			break;
+		  case'\\':
+			v.append('\\');
+			break;
+		  case'"':
+			v.append('\"');
+			break;
+		  case'\'':
+			v.append('\'');
+			break;
+		  case'0':
+			v.append(0);
+			break;
+		  default:
+			System.err.println("Unrecognized escape code in char literal");
+			error(c,ln,cn);
+			return new Token(TokenClass.INVALID,ln,cn);
+		  }
+		  break;
+		default:
+		  v.append(c);
+		}
+	  }
+	  System.err.println("Unclosed char literal");
+	  error(c,ln,cn);
+	  return new Token(TokenClass.INVALID,ln,cn);
+	case'0':
+	case'1':
+	case'2':
+	case'3':
+	case'4':
+	case'5':
+	case'6':
+	case'7':
+	case'8':
+	case'9':
+	  v=new StringBuilder();
+	  v.append(c);
+	  while(sc.hasNext()&&Character.isJavaIdentifierPart(sc.peek())){
+		c=sc.next();
+		v.append(c);
+		if(!Character.isDigit(c)){
+		  System.err.println("Invalid number");
+		  error(c,ln,cn);
+		  return new Token(TokenClass.INVALID,v.toString(),ln,cn);
+		}
+	  }
+	  return new Token(TokenClass.INT_LITERAL,v.toString(),ln,cn);
+	default://identifier or keyword
+	  if(!Character.isJavaIdentifierStart(c)||c=='$'){//unrecognized eg. ^
+		error(c,ln,cn);
+		return new Token(TokenClass.INVALID,ln,cn);
+	  }
+	  v=new StringBuilder();
+	  v.append(c);
+	  while(sc.hasNext()&&Character.isJavaIdentifierPart(sc.peek())){
+		v.append(c=sc.next());
+		if(c=='$'){//allowed by java identifier, disallowed by grammar spec
+		  error(c,ln,cn);
+		  return new Token(TokenClass.INVALID,v.toString(),ln,cn);
+		}
+	  }
+	  String s=v.toString();
+	  //todo check if keyword
+	  if(s.equals("int"))
+		return new Token(TokenClass.INT,ln,cn);
+	  if(s.equals("void"))
+		return new Token(TokenClass.VOID,ln,cn);
+	  if(s.equals("char"))
+		return new Token(TokenClass.CHAR,ln,cn);
+	  if(s.equals("if"))
+		return new Token(TokenClass.IF,ln,cn);
+	  if(s.equals("else"))
+		return new Token(TokenClass.ELSE,ln,cn);
+	  if(s.equals("while"))
+		return new Token(TokenClass.WHILE,ln,cn);
+	  if(s.equals("return"))
+		return new Token(TokenClass.RETURN,ln,cn);
+	  if(s.equals("struct"))
+		return new Token(TokenClass.STRUCT,ln,cn);
+	  if(s.equals("sizeof"))
+		return new Token(TokenClass.SIZEOF,ln,cn);
+	  return new Token(TokenClass.IDENTIFIER,s,ln,cn);
+	}
   }
 }
--- src/java/util/Position.java
+++ src/java/util/Position.java
@@ -1,22 +1,16 @@
 package util;
-
-
 /**
  * @author cdubach
  */
-public class Position {
-
-    final int line;
-    final int column;
-
-    public Position(int line, int column) {
-        this.line = line;
-        this.column = column;
-    }
-
-    @Override
-    public String toString() {
-        return line+":"+column;
-    }
-
+public class Position{
+  final int line;
+  final int column;
+  public Position(int line,int column){
+	this.line=line;
+	this.column=column;
+  }
+  @Override
+  public String toString(){
+	return line+":"+column;
+  }
 }
